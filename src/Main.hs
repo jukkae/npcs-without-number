5262ef902a875a24ee135a409f6afb54f732de07
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-18 15:39:58 +0200

    nDx dice

--- src/Main.hs
+++ src/Main.hs
@@ -7,11 +7,21 @@ npc :: Int -> Npc
 npc x =
   Npc "enemy" x
 
+npcFromType :: Int -> Npc
+npcFromType 0 = Npc "elite fighter" 3
+npcFromType 1 = Npc "basic fighter" 2
+npcFromType x = npc x
+
+-- roll nDx dice
+dice :: (RandomGen g) => Int -> Int -> g -> [Int]
+dice n x = take n . randomRs (1, x)
+
 main :: IO ()
 main = do
   g <- newStdGen -- seed random generator
   putStrLn ("enter an integer for enemy type")
   input <- getLine
   let x = (read input :: Int)
-  putStrLn ("NPC: " ++ name (npc x) ++ ", hp: " ++ show (hp (npc x)))
-  print $ take 1 (randomRs ('1', '6') g)
+  let n = npcFromType x
+  putStrLn ("NPC: " ++ name n ++ ", hp: " ++ show (hp n ))
+  print $ dice 1 6 g
