6988fafc42631aefb1b624937f2e7a52cda26799
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-18 11:51:17 +0200

    input parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,10 @@
-main = putStrLn "hello, world"
+npc :: Int -> [Char]
+npc x =
+  "enemy"
+
+main :: IO ()
+main = do
+  putStrLn ("enter an integer for enemy type")
+  input <- getLine
+  let x = (read input :: Int)
+  putStrLn ("NPC: " ++ npc x)
