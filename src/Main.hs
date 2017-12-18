9eb2b9022b956f8e171c72069aad7ebfa1cd1a68
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-18 12:30:34 +0200

    start work on actually defining npcs

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,13 @@
-npc :: Int -> [Char]
+data Npc = Npc { name :: String,
+                   hp :: Int } deriving (Show)
+
+npc :: Int -> Npc
 npc x =
-  "enemy"
+  Npc "enemy" x
 
 main :: IO ()
 main = do
   putStrLn ("enter an integer for enemy type")
   input <- getLine
   let x = (read input :: Int)
-  putStrLn ("NPC: " ++ npc x)
+  putStrLn ("NPC: " ++ name (npc x) ++ ", hp: " ++ show (hp (npc x)))
