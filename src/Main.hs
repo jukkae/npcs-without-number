383a18f24c5d381ad4783bef3bd99a33b00a72ef
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-03-10 15:29:05 +0200

    add description

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,40 @@
 import System.Random
 import Control.Monad
 
-data Npc = Npc {   kind :: String
-               ,     hp :: Int
-               ,     ac :: Int
-               ,    atk :: Int
-               ,    dmg :: Int
-               ,   move :: Int
-               , morale :: Int
-               , skills :: Int
-               ,  saves :: Int }
+data Npc = Npc {        kind :: String
+               ,          hp :: Int
+               ,          ac :: Int
+               ,         atk :: Int
+               ,         dmg :: String
+               ,        move :: Int
+               ,      morale :: Int
+               ,      skills :: Int
+               ,       saves :: Int
+               , description :: String }
 
-npc :: String -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Npc
+npc :: String -> Int -> Int -> Int -> String -> Int -> Int -> Int -> Int -> String -> Npc
 npc n x = Npc n x
 
 showNpc :: Npc -> String
-showNpc (Npc a b c d e f g h i) = "NPC: " ++ a
+showNpc (Npc a b c d e f g h i j) = "NPC: " ++ a
               ++ ", hp: " ++ show (b)
               ++ ", ac: " ++ show (c)
               ++ ", atk: +" ++ show (d)
-              ++ ", dmg: " ++ show (e)
+              ++ ", dmg: " ++ e
               ++ ", move: " ++ show (f) ++ "m"
               ++ ", morale: " ++ show (g)
               ++ ", skills: +" ++ show (h)
               ++ ", saves: " ++ show (i) ++ "+"
+              ++ "\n" ++ j
 
 instance Show Npc where show = showNpc
 
 -- hit dice = 6
-npcWithRandomHp 0 g = npc "elite fighter" (sum $ dice 3 6 g) 16 4 9 10 10 2 14
-npcWithRandomHp 1 g = npc "basic fighter" (sum $ dice 2 6 g) 14 2 6 10 9  1 14
-npcWithRandomHp x g = npc "enemy"         (sum $ dice x 6 g) 10 0 3 10 8  1 15
+npcWithRandomHp 0 g = npc "peaceful human" (sum $ dice 1 6 g) 10 0 "1d2" 10 6  1 15 "an unarmed ordinary person"
+npcWithRandomHp 1 g = npc "martial human" (sum $ dice 2 6 g) 10 1 "1d4" 10 8  1 15 "someone's been going to the gym, and has a knife"
+npcWithRandomHp 2 g = npc "veteran fighter" (sum $ dice 1 6 g) 14 2 "1d6+1" 10 9  1 14 "an unarmed ordinary person"
+npcWithRandomHp 3 g = npc "elite fighter" (sum $ dice 3 6 g) 16 4 "1d6+1" 10 10 2 14 "a gun for hire, wearing combat armor"
+npcWithRandomHp 4 g = npc "heroic fighter" (sum $ dice 6 6 g) 16 8 "1d8+3" 10 11 3 12 "a trained killer, wearing combat armor"
 
 -- roll nDx dice
 dice :: (RandomGen g) => Int -> Int -> g -> [Int]
@@ -41,7 +45,7 @@ main = do
   putStrLn ("How many enemies do you want?")
   input <- getLine
   let n = (read input :: Int)
-  putStrLn ("Enter an integer for enemy type [0-2]")
+  putStrLn ("Enter an integer for enemy difficulty [0-4]")
   input <- getLine
   let x = (read input :: Int)
   replicateM_ n $ do
